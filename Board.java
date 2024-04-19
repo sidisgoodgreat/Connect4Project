8d2c2ecd173c4a06c56470b92127651f84fa34d2
notSharkBoi <149710722+notSharkBoi@users.noreply.github.com>
2024-04-18 18:04:00 -0700

    Update Board.java
    Added Cardinal Directions(vertical and horizontal) win condition checking methods

--- Board.java
+++ Board.java
@@ -35,4 +35,38 @@ public class Board {
 		}
 		return false;
 	}
+	public int cardinalWin() {
+		//First checking horizontal wins(4 in a horizontal line)
+		for(int i=0;i<=5;i++) {//Going Row by Row
+			for(int j=0;j<=3;j++) {//first 3 columns only
+				if(cardinalCheck(i,j,true)) {
+					return board[i][j];
+				}
+			}
+		}
+		//If no Horizontals are found, searching for vertical wins
+		for(int i=5;i>=3;i--) {//Row
+			for(int j=6;j>=0;j--) {//Column
+				if(cardinalCheck(i,j,false)) {
+					return board[i][j];
+				}
+			}
+		}
+		return -1;//In the event no win is found, returns -1
+	}
+	private boolean cardinalCheck(int row, int column,boolean isHoz) {
+		int value=board[row][column];
+		return(value!=0&&recCardinal(row,column,value,0,isHoz));
+	}
+	private boolean recCardinal(int row, int column, int value,int count,boolean isHoz) {
+		if(count==4) {
+			return true;
+		}else if(board[row][column]==value) {
+			boolean val = isHoz?recCardinal(row,column+1,value,count+1,isHoz):
+									recCardinal(row-1,column,value,count+1,isHoz);
+			return val;
+		}else {
+			return false;
+		}
+	}
 }
