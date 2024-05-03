a1ffedd30bdc1191c830f9b5fd41119451610967
notSharkBoi <149710722+notSharkBoi@users.noreply.github.com>
2024-05-03 11:15:25 -0700

    Create Game.java
    placeholder "game" class that will have the necessary objects. Currently not working bc it uses the wrong GUIBoard object and such, but should work as an example of where to go after this.

---
+++ Game.java
@@ -0,0 +1,35 @@
+package csaFinal;
+import java.awt.Color;
+import java.util.Scanner;
+
+public class Game {
+	private GUIBoard board;
+	private Player p1,p2;
+	
+	public Game() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Choose Color?");
+		
+		String choice = s.next();
+		
+		Color p1Color,p2Color;
+		
+		if(choice.equals("y")) {
+			System.out.println("Player 1, Choose RGB Values(press enter after each value");
+			
+			p1Color = new Color(s.nextInt(),s.nextInt(),s.nextInt());
+			
+			System.out.println("Player 2, Choose RGB Values(press enter after each value");
+			
+			p2Color = new Color(s.nextInt(),s.nextInt(),s.nextInt());
+		}else {
+			p1Color=Color.red;
+			p2Color=Color.yellow;
+		}
+		p1=new Player(p1Color,1);
+		p2=new Player(p2Color,2);
+		
+		board=new GUIBoard(p1,p2);
+	}
+	
+}
