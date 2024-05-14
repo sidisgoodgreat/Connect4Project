ae4df6f466da7eb0cc4207d482bddd09addf4251
slickcapture <150636245+slickcapture@users.noreply.github.com>
2024-05-14 08:54:37 -0700

    Update Opponent.java

--- Opponent.java
+++ Opponent.java
@@ -4,13 +4,22 @@ import java.awt.Color;
 import java.util.Random;
 
 public class Opponent extends Player {
+    /**
+ 	*Creates the board instance variable
+  	*/
     private Board board;
 
+    /**
+ 	*Constructor initializing the color, id, and the board
+  	*/
     public Opponent(Color c, int id, Board board) {
         super(c, id);
         this.board = board;
     }
 
+    /**
+ 	*chooses the columnn based on the randomMove method
+  	*/
     public int chooseColumn() {
         int strategicMove = findStrategicMove();
         if (strategicMove != -1) {
@@ -19,6 +28,9 @@ public class Opponent extends Player {
         return randomMove();
     }
 
+    /**
+ 	*The piece will get dropped in a random column in the connect four grid.
+  	*/
     private int randomMove() {
         Random rand = new Random();
         int col;
@@ -28,6 +40,11 @@ public class Opponent extends Player {
         return col;
     }
 
+    /**
+ 	* finds a strategic move for the player to add a piece to the grid.
+    * traverse through the 2d array to find the specific value equivalent to 0
+    * return the correct value if applicable otherwise return -1
+  	*/
     private int findStrategicMove() {
         int maxSequence = 3;  
         for (int col = 0; col < board.getBoard()[0].length; col++) {
@@ -53,6 +70,9 @@ public class Opponent extends Player {
         return -1; 
     }
 
+    /**
+ 	*this method countSequence is used to count the number of consecutive occurences of a specific playerID in a particular direction from (row, col)
+  	*/
     private int countSequence(int row, int col, int playerID, int targetCount, int directionID) {
         int count = 1;  
         int rowAdd = (directionID == 1) ? 0 : (directionID == 2) ? 1 : (directionID == 3) ? 1 : 1;
@@ -74,9 +94,15 @@ public class Opponent extends Player {
         return count - 1;  
     }
 
+    /**
+ 	*this method isValidPosition checks if the position is a valid position in the game.
+  	*/
     private boolean isValidPosition(int row, int col) {
         return row >= 0 && row < board.getBoard().length && col >= 0 && col < board.getBoard()[0].length;
     }  
+    /**
+ 	*this method algorithimn is the explore different states of the game.
+  	*/
     public int algorithimn(int depth, int alpha, int beta, boolean maxPlayer) {
           //This stuf rlly complicated
     }
