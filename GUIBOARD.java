a6a905c5617ad227f8c33cea2cf7aacb2f9ce870
shotime2 <167370301+shotime2@users.noreply.github.com>
2024-05-14 09:43:08 -0700

    Update GUIBOARD.java

--- GUIBOARD.java
+++ GUIBOARD.java
@@ -33,6 +33,7 @@ public class GUIBoard1 implements ActionListener{
 	 */
 	/**
 	 * @wbp.parser.constructor
+  	*Constructs a GUI1 Board object
 	 */
 	public GUIBoard1() {
 		
@@ -43,6 +44,7 @@ public class GUIBoard1 implements ActionListener{
 		createDisplay();
 		
 	}
+	
 	public GUIBoard1(Player p1, Player p2) {
 		this.p1 = p1;
 		this.p2 = p2;
