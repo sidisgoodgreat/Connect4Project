045b89f278e52be20b7da7dc456717692f5edd3d
sidisgoodgreat <98197031+sidisgoodgreat@users.noreply.github.com>
2024-05-15 18:57:59 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -10,37 +10,26 @@ public class Player {
 	private int wins=0;
 	private Color playerCol;
 	private int id;
-
-	/** Constructor initializes id and the color
-	*
-	*/
+	
 	public Player(Color c, int id) {
 		this.id=id;
 		this.playerCol=c;
 	}
-	/**
-	*Increments wins by 1 if player wins
-	*/
+	
 	public void incWin() {
 		wins++;
 	}
-	/** 
-	*Returns the number of times the player won
-	*/
 	public int getWins() {
 		return wins;
 	}
-	/**
-	*Returns the color of the player
-	*/
 	public Color getColor() {
 		return playerCol;
 	}
-	/**
-	*Returns the id
-	*/
 	public int getID() {
 		return id;
 	}
 	
+	public int chooseColumn() {
+		return 1;
+	}
 }
