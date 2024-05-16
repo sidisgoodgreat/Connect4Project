fd3cf752ca22499c32337ada3affc4d31813faf4
sidisgoodgreat <98197031+sidisgoodgreat@users.noreply.github.com>
2024-05-15 20:29:10 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,34 +2,62 @@ package CSAFinalProject;
 
 import java.awt.Color;
 
+/**
+ * This class represents a player in the game.
+ * A player has an ID, a color, and a count of wins.
+ */
 public class Player {
-	//Player class should have a number/id set to them
-	//A color associated with them
-	//Points based on how many times they've won
-	
-	private int wins=0;
-	private Color playerCol;
-	private int id;
-	
-	public Player(Color c, int id) {
-		this.id=id;
-		this.playerCol=c;
-	}
-	
-	public void incWin() {
-		wins++;
-	}
-	public int getWins() {
-		return wins;
-	}
-	public Color getColor() {
-		return playerCol;
-	}
-	public int getID() {
-		return id;
-	}
-	
-	public int chooseColumn() {
-		return 1;
-	}
+    private int wins = 0;
+    private Color playerCol;
+    private int id;
+
+    /**
+     * Constructs a Player with a specified color and ID.
+     * @param c the color of the player
+     * @param id the ID of the player
+     */
+    public Player(Color c, int id) {
+        this.id = id;
+        this.playerCol = c;
+    }
+
+    /**
+     * Increments the win count for the player.
+     */
+    public void incWin() {
+        wins++;
+    }
+
+    /**
+     * Gets the number of wins for the player.
+     * @return the number of wins
+     */
+    public int getWins() {
+        return wins;
+    }
+
+    /**
+     * Gets the color of the player.
+     * @return the color
+     */
+    public Color getColor() {
+        return playerCol;
+    }
+
+    /**
+     * Gets the ID of the player.
+     * @return the ID
+     */
+    public int getID() {
+        return id;
+    }
+
+    /**
+     * Chooses a column for the player's move.
+     * This method can be overridden by subclasses for specific behaviors.
+     * @return the column number (default is 1)
+     */
+    public int chooseColumn() {
+        return 1;
+    }
 }
